adb3459a9f783a38b33c25341f46c68acdf5aa6d
Luis daniel rovira contreras <luisdanielrovira8@gmail.com>
2021-07-06 21:38:11 +0200

    change dist god

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   target: 'static',
-  dir: 'docs',
+  generate: {
+    dir: 'docs',
+  },
   head: {
     title: 'iconsax-docs',
     htmlAttrs: {
