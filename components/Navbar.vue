7f83b8d3a673551e961c922d3a769718ff815a50
Luis daniel rovira contreras <luisdanielrovira8@gmail.com>
2021-07-06 21:21:26 +0200

    push

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navbar">
+        iconsax
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style lang="sass">
+.navbar
+    background: -color('color')
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="landing">
+    <Navbar />
+    Hello world
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
